bbec4e5a684185281b77091cdee4a77f1f4f8f30
Xart3mis <ihavemanywaifus@protonmail.com>
2022-10-03 23:15:32 +0200

    make it seem like multiple clients

--- client/client.go
+++ client/client.go
@@ -13,15 +13,19 @@ import (
 
 func main() {
 	// Set up connection with the grpc server
-	conn, err := grpc.Dial("localhost:8000", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	conn, err := grpc.Dial("0.0.0.0:8000", grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		log.Fatalf("Error while making connection, %v", err)
 	}
 
 	// Create a client instance
 	c := pb.NewConsumerClient(conn)
+	c.RegisterClient(context.Background(), &pb.ClientDataRequest{ClientId: "id_1"})
 
 	for {
-		fmt.Println(c.UpdateClients(context.Background(), &pb.ClientDataRequest{ClientId: "1"}))
+		for i := 0; i < 10; i++ {
+			fmt.Println(c.GetOnScreenText(context.Background(),
+				&pb.ClientDataRequest{ClientId: fmt.Sprint(i)}))
+		}
 	}
 }
