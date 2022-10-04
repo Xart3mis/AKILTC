96e7eda7e5533fab4eb663b343085670a6d81b5f
Xart3mis <ihavemanywaifus@protonmail.com>
2022-10-04 12:34:16 +0200

    rename UnRegisterClient to UnregisterClient

--- client_data_pb/ClientData_grpc.pb.go
+++ client_data_pb/ClientData_grpc.pb.go
@@ -22,7 +22,7 @@ const _ = grpc.SupportPackageIsVersion7
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
 type ConsumerClient interface {
-	UnRegisterClient(ctx context.Context, in *ClientDataRequest, opts ...grpc.CallOption) (*RegisterResponse, error)
+	UnregisterClient(ctx context.Context, in *ClientDataRequest, opts ...grpc.CallOption) (*RegisterResponse, error)
 	RegisterClient(ctx context.Context, in *ClientDataRequest, opts ...grpc.CallOption) (*RegisterResponse, error)
 	GetOnScreenText(ctx context.Context, in *ClientDataRequest, opts ...grpc.CallOption) (*ClientDataOnScreenTextResponse, error)
 }
@@ -35,9 +35,9 @@ func NewConsumerClient(cc grpc.ClientConnInterface) ConsumerClient {
 	return &consumerClient{cc}
 }
 
-func (c *consumerClient) UnRegisterClient(ctx context.Context, in *ClientDataRequest, opts ...grpc.CallOption) (*RegisterResponse, error) {
+func (c *consumerClient) UnregisterClient(ctx context.Context, in *ClientDataRequest, opts ...grpc.CallOption) (*RegisterResponse, error) {
 	out := new(RegisterResponse)
-	err := c.cc.Invoke(ctx, "/client_data_pb.Consumer/UnRegisterClient", in, out, opts...)
+	err := c.cc.Invoke(ctx, "/client_data_pb.Consumer/UnregisterClient", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
@@ -66,7 +66,7 @@ func (c *consumerClient) GetOnScreenText(ctx context.Context, in *ClientDataRequ
 // All implementations must embed UnimplementedConsumerServer
 // for forward compatibility
 type ConsumerServer interface {
-	UnRegisterClient(context.Context, *ClientDataRequest) (*RegisterResponse, error)
+	UnregisterClient(context.Context, *ClientDataRequest) (*RegisterResponse, error)
 	RegisterClient(context.Context, *ClientDataRequest) (*RegisterResponse, error)
 	GetOnScreenText(context.Context, *ClientDataRequest) (*ClientDataOnScreenTextResponse, error)
 	mustEmbedUnimplementedConsumerServer()
@@ -76,8 +76,8 @@ type ConsumerServer interface {
 type UnimplementedConsumerServer struct {
 }
 
-func (UnimplementedConsumerServer) UnRegisterClient(context.Context, *ClientDataRequest) (*RegisterResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method UnRegisterClient not implemented")
+func (UnimplementedConsumerServer) UnregisterClient(context.Context, *ClientDataRequest) (*RegisterResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method UnregisterClient not implemented")
 }
 func (UnimplementedConsumerServer) RegisterClient(context.Context, *ClientDataRequest) (*RegisterResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method RegisterClient not implemented")
@@ -98,20 +98,20 @@ func RegisterConsumerServer(s grpc.ServiceRegistrar, srv ConsumerServer) {
 	s.RegisterService(&Consumer_ServiceDesc, srv)
 }
 
-func _Consumer_UnRegisterClient_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+func _Consumer_UnregisterClient_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(ClientDataRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(ConsumerServer).UnRegisterClient(ctx, in)
+		return srv.(ConsumerServer).UnregisterClient(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/client_data_pb.Consumer/UnRegisterClient",
+		FullMethod: "/client_data_pb.Consumer/UnregisterClient",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(ConsumerServer).UnRegisterClient(ctx, req.(*ClientDataRequest))
+		return srv.(ConsumerServer).UnregisterClient(ctx, req.(*ClientDataRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
@@ -160,8 +160,8 @@ var Consumer_ServiceDesc = grpc.ServiceDesc{
 	HandlerType: (*ConsumerServer)(nil),
 	Methods: []grpc.MethodDesc{
 		{
-			MethodName: "UnRegisterClient",
-			Handler:    _Consumer_UnRegisterClient_Handler,
+			MethodName: "UnregisterClient",
+			Handler:    _Consumer_UnregisterClient_Handler,
 		},
 		{
 			MethodName: "RegisterClient",
